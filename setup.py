7ee0cd09f4159f35b7f66d0290f6e46bb11705cc
Al Sweigart <asweigart@gmail.com>
2019-06-17 19:50:00 -0700

    Updated setup.py to a more modern format (using my cookiecutter-basicpythonproject template) which also solved the issue of not being able to install pymsgbox with 'pip install -e .'

--- setup.py
+++ setup.py
@@ -1,39 +1,38 @@
+import re
+from setuptools import setup, find_packages
 
-from setuptools import setup
+# Load version from module (without loading the whole module)
+with open('src/pymsgbox/__init__.py', 'r') as fo:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fo.read(), re.MULTILINE).group(1)
 
-
-# Dynamically calculate the version based on pyautogui.VERSION.
-version = __import__('pymsgbox').__version__
-
+# Read in the README.md for the long description.
+with open('README.md') as fo:
+    long_description = fo.read()
 
 setup(
     name='PyMsgBox',
     version=version,
-    url='https://github.com/asweigart/PyMsgBox',
+    url='https://github.com/asweigart/pymsgbox',
     author='Al Sweigart',
     author_email='al@inventwithpython.com',
-    description=('A simple, cross-platform, pure Python module for JavaScript-like message boxes.'),
-    license='BSD',
-    packages=['pymsgbox'],
+    description=('''A simple, cross-platform, pure Python module for JavaScript-like message boxes.'''),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='GPLv3+',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     test_suite='tests',
-    keywords="gui msgbox message box dialog confirmation confirm password alert",
+    install_requires=[],
+    keywords='',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Win32 (MS Windows)',
-        'Environment :: X11 Applications',
-        'Environment :: MacOS X',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
 )
