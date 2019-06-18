71e28a667dff01694ce2d094c14dd141ae0f35e6
Al Sweigart <asweigart@gmail.com>
2019-06-17 22:34:14 -0700

    Extended the tests to check native and tkinter versions of the alert() and confirm() tests.

--- tests/test_pymsgbox.py
+++ tests/test_pymsgbox.py
@@ -42,184 +42,228 @@ class KeyPresses(threading.Thread):
 
 class AlertTests(unittest.TestCase):
     def test_alert(self):
-        # no text
-        t = KeyPresses(['enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.alert(), 'OK')
+        for func in (pymsgbox._alertTkinter, pymsgbox.alert):
+            if func is pymsgbox._alertTkinter:
+                print('Testing tkinter alert()')
+            elif func is pymsgbox.alert:
+                print('Testing native alert()')
+            # no text
+            t = KeyPresses(['enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func(), 'OK')
 
-        # text
-        t = KeyPresses(['enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.alert('Hello'), 'OK')
+            # text
+            t = KeyPresses(['enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello'), 'OK')
 
-        # text and title
-        t = KeyPresses(['enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.alert('Hello', 'Title'), 'OK')
+            # text and title
+            t = KeyPresses(['enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello', 'Title'), 'OK')
 
-        # text, title, and custom button
-        t = KeyPresses(['enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.alert('Hello', 'Title', 'Button'), 'Button')
+            # text, title, and custom button
+            t = KeyPresses(['enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello', 'Title', 'Button'), 'Button')
 
-        # using keyword arguments
-        t = KeyPresses(['enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.alert(text='Hello', title='Title', button='Button'), 'Button')
+            # using keyword arguments
+            t = KeyPresses(['enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func(text='Hello', title='Title', button='Button'), 'Button')
 
 
 class ConfirmTests(unittest.TestCase):
     def test_confirm(self):
-        # press enter on OK
-        t = KeyPresses(['enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm(), 'OK')
+        for func in (pymsgbox._confirmTkinter, pymsgbox.confirm):
+            if func is pymsgbox._confirmTkinter:
+                print('Testing tkinter confirm()')
+            elif func is pymsgbox.confirm:
+                print('Testing native confirm()')
 
-        # press right, enter on Cancel
-        t = KeyPresses(['right', 'enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm(), 'Cancel')
+            # press enter on OK
+            t = KeyPresses(['enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func(), 'OK')
 
-        # press right, left, right, enter on Cancel
-        t = KeyPresses(['right', 'left', 'right', 'enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm(), 'Cancel')
+            # press right, enter on Cancel
+            t = KeyPresses(['right', 'enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func(), 'Cancel')
 
-        # press tab, enter on Cancel
-        t = KeyPresses(['tab', 'enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm(), 'Cancel')
+            # press right, left, right, enter on Cancel
+            t = KeyPresses(['right', 'left', 'right', 'enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func(), 'Cancel')
 
-        # press tab, tab, enter on OK
-        t = KeyPresses(['tab', 'tab', 'enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm(), 'OK')
+            # press tab, enter on Cancel
+            t = KeyPresses(['tab', 'enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func(), 'Cancel')
 
-        # with text
-        t = KeyPresses(['enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm('Hello'), 'OK')
+            # press tab, tab, enter on OK
+            t = KeyPresses(['tab', 'tab', 'enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func(), 'OK')
 
-        # with text, title
-        t = KeyPresses(['enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm('Hello', 'Title'), 'OK')
+            # with text
+            t = KeyPresses(['enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello'), 'OK')
 
-        # with text, title, and one custom button
-        t = KeyPresses(['enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm('Hello', 'Title', ['A']), 'A')
+            # with text, title
+            t = KeyPresses(['enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello', 'Title'), 'OK')
 
-        # with text, title, and one custom blank button
-        t = KeyPresses(['enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm('Hello', 'Title', ['']), '')
+            # with text, title, and one custom button
+            t = KeyPresses(['enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello', 'Title', ['A']), 'A')
 
-        # with text, title, and two custom buttons
-        t = KeyPresses(['enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm('Hello', 'Title', ['A', 'B']), 'A')
+            # with text, title, and one custom blank button
+            t = KeyPresses(['enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello', 'Title', ['']), '')
 
-        t = KeyPresses(['right', 'enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm('Hello', 'Title', ['A', 'B']), 'B')
+            # with text, title, and two custom buttons
+            t = KeyPresses(['enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello', 'Title', ['A', 'B']), 'A')
 
-        t = KeyPresses(['right', 'left', 'enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm('Hello', 'Title', ['A', 'B']), 'A')
+            t = KeyPresses(['right', 'enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello', 'Title', ['A', 'B']), 'B')
 
-        t = KeyPresses(['tab', 'enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm('Hello', 'Title', ['A', 'B']), 'B')
+            t = KeyPresses(['right', 'left', 'enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello', 'Title', ['A', 'B']), 'A')
 
-        t = KeyPresses(['tab', 'tab', 'enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm('Hello', 'Title', ['A', 'B']), 'A')
+            t = KeyPresses(['tab', 'enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello', 'Title', ['A', 'B']), 'B')
 
-        # with text, title, and three custom buttons
-        t = KeyPresses(['tab', 'tab', 'enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm('Hello', 'Title', ['A', 'B', 'C']), 'C')
+            t = KeyPresses(['tab', 'tab', 'enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello', 'Title', ['A', 'B']), 'A')
 
-        # with text, title, and four custom buttons
-        t = KeyPresses(['tab', 'tab', 'tab', 'enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm('Hello', 'Title', ['A', 'B', 'C', 'D']), 'D')
+            # with text, title, and three custom buttons
+            t = KeyPresses(['tab', 'tab', 'enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello', 'Title', ['A', 'B', 'C']), 'C')
 
-        # with text, title, and five custom buttons
-        t = KeyPresses(['tab', 'tab', 'tab', 'tab', 'enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm('Hello', 'Title', ['A', 'B', 'C', 'D', 'E']), 'E')
+            # with text, title, and four custom buttons
+            t = KeyPresses(['tab', 'tab', 'tab', 'enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello', 'Title', ['A', 'B', 'C', 'D']), 'D')
 
-        # with text, title, and three custom buttons specified with keyword arguments
-        t = KeyPresses(['tab', 'tab', 'enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm(text='Hello', title='Title', buttons=['A', 'B', 'C']), 'C')
+            # with text, title, and five custom buttons
+            t = KeyPresses(['tab', 'tab', 'tab', 'tab', 'enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func('Hello', 'Title', ['A', 'B', 'C', 'D', 'E']), 'E')
 
-        # test that pressing Esc is the same as clicking Cancel (but only when there is a cancel button)
-        t = KeyPresses(['escape'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm(text='Escape button press test'), 'Cancel')
+            # with text, title, and three custom buttons specified with keyword arguments
+            t = KeyPresses(['tab', 'tab', 'enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func(text='Hello', title='Title', buttons=['A', 'B', 'C']), 'C')
 
-        # Make sure that Esc keypress does nothing if there is no Cancel button.
-        t = KeyPresses(['escape', 'enter'])
-        t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual(pymsgbox.confirm(text='Escape button press test', buttons=['OK', 'Not OK']), 'OK')
+            # test that pressing Esc is the same as clicking Cancel (but only when there is a cancel button)
+            t = KeyPresses(['escape'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func(text='Escape button press test'), 'Cancel')
+
+            # Make sure that Esc keypress does nothing if there is no Cancel button.
+            t = KeyPresses(['escape', 'enter'])
+            t.start()
+            print('Line', inspect.currentframe().f_lineno)
+            self.assertEqual(func(text='Escape button press test', buttons=['OK', 'Not OK']), 'OK')
 
 class PromptPasswordTests(unittest.TestCase):
     def test_prompt(self):
-        self._prompt_and_password_tests(pymsgbox.prompt, 'prompt()')
+        self._prompt_and_password_tests(pymsgbox._promptTkinter, 'prompt()')
 
     def test_password(self):
         # NOTE: Currently there is no way to test the appearance of the * or custom mask characters.
-        self._prompt_and_password_tests(pymsgbox.password, 'password()')
+        self._prompt_and_password_tests(pymsgbox._passwordTkinter, 'password()')
 
     def _prompt_and_password_tests(self, msgBoxFunc, msgBoxFuncName):
         # entering nothing
         t = KeyPresses(['enter'])
         t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual((msgBoxFuncName, msgBoxFunc()), (msgBoxFuncName, ''))
+        print('Line', inspect.currentframe().f_lineno)
+        self.assertEqual((msgBoxFuncName, msgBoxFunc()), (msgBoxFuncName, ''))
 
         # entering text
         t = KeyPresses(['a', 'b', 'c', 'enter'])
         t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual((msgBoxFuncName, msgBoxFunc()), (msgBoxFuncName, 'abc'))
+        print('Line', inspect.currentframe().f_lineno)
+        self.assertEqual((msgBoxFuncName, msgBoxFunc()), (msgBoxFuncName, 'abc'))
 
         # entering text, tabbing to the Ok key
         t = KeyPresses(['a', 'b', 'c', 'tab', 'enter'])
         t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual((msgBoxFuncName, msgBoxFunc()), (msgBoxFuncName, 'abc'))
+        print('Line', inspect.currentframe().f_lineno)
+        self.assertEqual((msgBoxFuncName, msgBoxFunc()), (msgBoxFuncName, 'abc'))
 
         # entering text but hitting cancel
         t = KeyPresses(['a', 'b', 'c', 'tab', 'tab', 'enter'])
         t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual((msgBoxFuncName, msgBoxFunc()), (msgBoxFuncName, None))
+        print('Line', inspect.currentframe().f_lineno)
+        self.assertEqual((msgBoxFuncName, msgBoxFunc()), (msgBoxFuncName, None))
 
         # with text
         t = KeyPresses(['a', 'b', 'c', 'enter'])
         t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual((msgBoxFuncName, msgBoxFunc('Hello')), (msgBoxFuncName, 'abc'))
+        print('Line', inspect.currentframe().f_lineno)
+        self.assertEqual((msgBoxFuncName, msgBoxFunc('Hello')), (msgBoxFuncName, 'abc'))
 
         # with text and title
         t = KeyPresses(['a', 'b', 'c', 'enter'])
         t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual((msgBoxFuncName, msgBoxFunc('Hello', 'Title')), (msgBoxFuncName, 'abc'))
+        print('Line', inspect.currentframe().f_lineno)
+        self.assertEqual((msgBoxFuncName, msgBoxFunc('Hello', 'Title')), (msgBoxFuncName, 'abc'))
 
         # with text, title and default value
         t = KeyPresses(['enter'])
         t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual((msgBoxFuncName, msgBoxFunc('Hello', 'Title', 'default')), (msgBoxFuncName, 'default'))
+        print('Line', inspect.currentframe().f_lineno)
+        self.assertEqual((msgBoxFuncName, msgBoxFunc('Hello', 'Title', 'default')), (msgBoxFuncName, 'default'))
 
         # with text, title and default value specified by keyword arguments
         t = KeyPresses(['enter'])
         t.start()
-        print('Line', inspect.currentframe().f_lineno); self.assertEqual((msgBoxFuncName, msgBoxFunc(text='Hello', title='Title', default='default')), (msgBoxFuncName, 'default'))
+        print('Line', inspect.currentframe().f_lineno)
+        self.assertEqual((msgBoxFuncName, msgBoxFunc(text='Hello', title='Title', default='default')), (msgBoxFuncName, 'default'))
 
 class TimeoutTests(unittest.TestCase):
     def test_timeout(self):
         # Note: If these test's fail, the unit tests will hang.
-        self.assertEqual(pymsgbox.alert('timeout test', timeout=1000), pymsgbox.TIMEOUT_RETURN_VALUE)
-        self.assertEqual(pymsgbox.confirm('timeout test', timeout=1000), pymsgbox.TIMEOUT_RETURN_VALUE)
+        self.assertEqual(pymsgbox._alertTkinter('timeout test', timeout=1000), pymsgbox.TIMEOUT_RETURN_VALUE)
+        self.assertEqual(pymsgbox._confirmTkinter('timeout test', timeout=1000), pymsgbox.TIMEOUT_RETURN_VALUE)
         self.assertEqual(pymsgbox.prompt('timeout test', timeout=1000), pymsgbox.TIMEOUT_RETURN_VALUE)
         self.assertEqual(pymsgbox.password('timeout test', timeout=1000), pymsgbox.TIMEOUT_RETURN_VALUE)
 
